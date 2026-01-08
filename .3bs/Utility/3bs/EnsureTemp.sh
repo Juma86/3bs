31f528e00ae599451cc7d2532c8388a2f4d57c03
Connor R Beeney <C.Beeney@outlook.com>
2026-01-08 19:22:30 +0000

    QoL Features.

--- .3bs/Utility/3bs/EnsureTemp.sh
+++ .3bs/Utility/3bs/EnsureTemp.sh
@@ -1,7 +1,8 @@
 _CreateTempIfNotExists() {
     current_dir="$(readlink -f "$(dirname "$0")")"
     temp_dir="$current_dir/.3bs/Temp"/"$1"
-    if [ ! -d "$temp_dir" ]; then
+
+    if [ ! -d "$temp_dir" ] && [ "$2" != "phony" ]; then
         mkdir -p "$temp_dir"
     fi
 
