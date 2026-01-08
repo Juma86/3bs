e71e09196286c5ca39a66f9f55bb5fc16a346478
Connor R Beeney <C.Beeney@outlook.com>
2026-01-08 17:47:00 +0000

    Fixed bugs.

--- .3bs/Utility/3bs/EnsureTemp.sh
+++ .3bs/Utility/3bs/EnsureTemp.sh
@@ -1,9 +1,9 @@
 _CreateTempIfNotExists() {
     current_dir="$(readlink -f "$(dirname "$0")")"
-    temp_dir="$current_dir/.3bs/Temp"
+    temp_dir="$current_dir/.3bs/Temp"/"$1"
     if [ ! -d "$temp_dir" ]; then
         mkdir -p "$temp_dir"
     fi
 
-    echo "$temp_dir/$1"
+    echo "$temp_dir"
 }
