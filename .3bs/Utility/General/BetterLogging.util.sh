33d45633848097f90670ac154c9f375b6f321302
Connor R Beeney <C.Beeney@outlook.com>
2026-01-10 02:16:29 +0000

    Updating Bugfixes

--- .3bs/Utility/General/BetterLogging.util.sh
+++ .3bs/Utility/General/BetterLogging.util.sh
@@ -9,21 +9,25 @@ source $here/$this/*.util.sh
 # 192 136 180
 _DisplayInfo() {
     _SetColour custom 100 190 180
-        echo -e "I: $1"
+        echo -ne "I: $1"
+        if [ "$2" != "--no-newline" ]; then echo -ne "\n"; fi
     _SetColour reset
 }
 _DisplayTrace() {
     _SetColour white
-        echo -e "T: $1"
+        echo -ne "T: $1"
+        if [ "$2" != "--no-newline" ]; then echo -ne "\n"; fi
     _SetColour reset
 }
 _DisplayWarning() {
     _SetColour custom 200 130 15
-        echo -e "W: $1"
+        echo -ne "W: $1"
+        if [ "$2" != "--no-newline" ]; then echo -ne "\n"; fi
     _SetColour reset
 }
 _DisplayError() {
     _SetColour red
-        echo -e "E: $1"
+        echo -ne "E: $1"
+        if [ "$2" != "--no-newline" ]; then echo -ne "\n"; fi
     _SetColour reset
 }
--- .3bs/Utility/General/BetterLogging/SetConsoleColor.util.sh
+++ .3bs/Utility/General/BetterLogging/SetConsoleColor.util.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-here="`readlink -f $(dirname ${BASH_SOURCE[0]})`"
-this="`basename ${BASH_SOURCE[0]} .util.sh`"
-
-
 _SetColour() {
     case $1 in
-        "white") echo -e "\033[0;37m" ;;
-        "red") echo -e "\033[0;31m" ;;
-        "yellow") echo -e "\033[0;33m" ;;
-        "orange") echo -e "\033[38;2;255;165;0m" ;;
-        "custom") echo -e "\033[38;2;$2;$3;${4}m" ;;
-        "reset") echo -e "\033[0m" ;;
-        *) echo "" ;;
+        "white") echo -ne "\033[0;37m" ;;
+        "red") echo -ne "\033[0;31m" ;;
+        "yellow") echo -ne "\033[0;33m" ;;
+        "orange") echo -ne "\033[38;2;255;165;0m" ;;
+        "custom") echo -ne "\033[38;2;$2;$3;${4}m" ;;
+        "reset") echo -ne "\033[0m" ;;
+        *) echo -ne "" ;;
     esac
 }
